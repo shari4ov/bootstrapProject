59ea5dc0232cb460f95c8f1274e588bc1a61d908
= <=>
2022-02-24 01:11:29 +0400

    first

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready( function () {
+       $('#myTable').DataTable();
+   } );
+
+document.querySelector('.navbar-toggler').addEventListener('click',function(){
+       document.getElementById('vertical-nav').style.display = 'block';
+})
